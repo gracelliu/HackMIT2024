e97da4d8c7f21795b4bfb393275599fef4ea7906
Lucas Reljic <lucas.reljic@gmail.com>
2024-09-15 10:50:28 -0400

    changed back url

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -15,7 +15,7 @@ extern "C" {
 
 
 
-const char* api_endpoint = "https://smee.io/ndZloBWBaaS7E1hO";
+const char* api_endpoint = "https://standing-starling-962.convex.site/storeData";
 const char* test_url = "https://www.google.com"; 
 Adafruit_MPU6050 mpu;
 StaticJsonDocument<200> doc;
