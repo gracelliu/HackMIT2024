046dcc04230928121fa53d439b01da5b5598ca38
Grace Liu <graceliu.gll@gmail.com>
2024-09-15 06:20:47 -0400

    buttons 🙏

--- my-app/src/Pages/EntryPage.jsx
+++ my-app/src/Pages/EntryPage.jsx
@@ -1,33 +1,35 @@
 // src/pages/EntryPage.jsx
 import React, { useState } from "react";
-
+import SideBar from "../SideBar.jsx"
 import MyLineChart from "../assets/MyLineChart";
 
 function EntryPage() {
 
   const [stress, setStress] = useState(0)
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', backgroundColor: '#FFFFFF', borderColor: "#FFFFFF", minHeight: '100vh', width: '100%', margin: '0', justifyContent: 'start' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', backgroundColor: '#FFFFFF', borderColor: "#FFFFFF", minHeight: '100vh', width: '100%', margin: '0', alignItems: "start", justifyContent: 'start' }}>
       <div>
-        
-        <h1 style={{color: '#000000', font: '90pt'}}>StressLess</h1>
+        {/* <SideBar></SideBar> */}
+        <div style={{ color: '#58315A', fontFamily: 'sans-serif', fontSize: '30pt', marginTop: '20px' }}>StressLess</div>
       </div>
-      <div style={{ width: '100%' }}></div>
-      
-      <h3 style={{color: '#000000'}}>  
+
+      <h3 style={{ color: '#58315A', marginBottom: '-10px', justifySelf: "start" }}>
         Overview for today
       </h3>
 
-      <img src="src/assets/stress_graph.png" style={{ width: '350px', height: '250px', marginLeft: '30px', marginBottom: '20px' }} />
+      <img src="src/assets/stress_graph.png" style={{
+        width: '350px', height: '220px'
+      }} />
 
-      <h3 style={{ color: '#000000' }}>
+      <h3 style={{ color: '#58315A' }}>
         Stress Index
       </h3>
+
       <div style={{
-        backgroundColor: '#FFEDC1', maxWidth:"350px", display: 'flex', alignItems: 'center', justifyContent: 'space-around',
-        borderRadius: '30px', margin:"10px, 10px", padding:"10px", color: 'white'
+        backgroundColor: '#FFEDC1', maxWidth: "350px", display: 'flex', alignItems: 'center', justifyContent: 'space-around',
+        borderRadius: '30px', margin: "10px, 10px, -100px, 10px", padding: "10px", color: 'white'
       }}>
-        <div style={{ color: '#000000', marginLeft: '30px' }}>
+        <div style={{ color: '#000000', marginLeft: '20px' }}>
           <p>
             Your stress index is based on factors such as fidgeting, heart rate, and body temperature.
           </p>
@@ -39,12 +41,11 @@ function EntryPage() {
         setStress(stress + 1)
       }}>press</button>  */}
 
-
-      <div style={{ flex: '2' }}>
+      <div style={{
+        alignSelf:
+          'center'
+      }}>
         <div style={{ color: "#FF0000" }}>
-
-
-
 
           <p style={{ color: '#000000', fontSize: '80pt' }}>
             {stress}
